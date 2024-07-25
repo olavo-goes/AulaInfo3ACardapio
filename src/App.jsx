9e94e11ee280fe8dcbc0f4fe826a139e494de750
olavo-goes <olavo.goes@estudante.ifms.edu.br>
2024-07-25 08:28:09 -0400

    novo repositorio
    Colando atividade feita em outro arquivo para salvar no git.

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,93 @@
 import "../src/globals.css";
-
+import { useState } from "react";
 
 export default function App() {
-   const [listaProdutos, setProdutos] = useState([
+    const [listaProdutos, setProdutos] = useState([
+
         {
+
             id: 1,
+
             item: "Hambúrguer",
+
             imagem: "https://www.assai.com.br/sites/default/files/shutterstock_1806472312.jpg",
+
             preco: "R$ 25,99"
+
         },
+
         {
+
             id: 2,
+
             item: "Coca-cola 350ml",
+
             imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm4S15squn95k7qtrVOpMX1MOJGe48y4B7FQ&s",
+
             preco: "R$ 5,99"
+
         },
+
         {
+
             id: 3,
+
             item: "Batatas fritas",
+
             imagem: "https://gastronomiacarioca.zonasul.com.br/wp-content/uploads/2023/05/batata_frita_destaque_ilustrativo_zona_sul.jpg",
+
             preco: "R$ 8,99"
+
         },
+
         {
+
             id: 4,
+
             item: "Suco de Frutas",
+
             imagem: "https://helenalunardelli.com.br/wp-content/uploads/2013/02/sucos.jpg",
+
             preco: "R$ 8,99"
+
         },
+
     ]);
+   
+    const [listaProdutosPedidos, setPedidos] = useState([])
+    const adicionarProdutoPedido = (produto) => {
+        setPedidos([...listaProdutosPedidos, produto])
+    }
+
+console.table(listaProdutosPedidos)
 
     return (
-       <div>
+       <div className="bloco-principal">
+        <div className="bloco-produtos">
+        {listaProdutos.map((produto) => <div key={produto.id}>
+            <img src={produto.imagem}/>
+             <p>{produto.item}</p>
+             <p>{produto.preco}</p>
+             <button onClick={() => adicionarProdutoPedido(produto)}>Pedir</button>
+            </div>)}
+       </div>
 
+    <div className="bloco-pedidos">
+        <p>meus pedidos</p>
+            {
+                listaProdutosPedidos.map((produto) => (
+                    <div key={produto.id}>
+                        <table>
+                            <tr>
+                                <td><img src={produto.imagem}></img></td>
+                                <td>{produto.item}</td>
+                                <td>{produto.preco}</td>
+                            </tr>
+                        </table>
+                    </div>
+                ))
+            }
+    </div>
        </div>
     );
 }
